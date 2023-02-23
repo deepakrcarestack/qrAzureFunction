65a01d62ad34b0f636aa20e68bb86a4e8f297873
Deepak R <deepakr@carestack.com>
2023-02-23 18:57:18 +0530

    update

--- SingPass/FunctionApp1/Function1.cs
+++ SingPass/FunctionApp1/Function1.cs
@@ -8,14 +8,14 @@ namespace FunctionApp1
 {
     public static class SingpassCallback
     {
-        [FunctionName("qrCallback/callback")]
+        [FunctionName("qrCallback")]
         public static  IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "qrCallback/callback")] HttpRequest req,ILogger log)
         {
             log.LogInformation("Received Code in staging"+ req.Query["code"]);
             return new OkResult();
         }
 
-        [FunctionName("qrCallbackProd/callback")]
+        [FunctionName("qrCallbackProd")]
         public static IActionResult RunProd([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "qrCallbackProd/callback")] HttpRequest req, ILogger log)
         {
             log.LogInformation("Received Code in prod" + req.Query["code"]);
