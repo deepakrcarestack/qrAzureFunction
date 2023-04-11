115633316f402f87c4ed000fc7130ae86d9ea069
Deepak R <deepakr@carestack.com>
2023-04-11 19:44:16 +0530

    update

--- SingPass/FunctionApp1/Function1.cs
+++ SingPass/FunctionApp1/Function1.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using sg.gov.ndi.MyInfoConnector;
+using System;
 
 namespace FunctionApp1
 {
@@ -12,7 +14,11 @@ namespace FunctionApp1
         [FunctionName("qrCallback")]
         public static IActionResult Run1([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "qrCallback")] HttpRequest req, ILogger log)
         {
-            log.LogInformation("Received Code" + req.Query["code"]);
+            log.LogInformation("Received Code: " + req.Query["code"] +" , "+ req.Query["state"]);
+
+            //var connector = MyInfoConnector.Create();
+            //var authoriseUrl = connector.GetAuthoriseUrl(redirectUrl);
+            //connector.GetPersonJson(authCode, state);
             return new OkResult();
         }
     }
