3ed83e0d4e594ffdb76f4bb97b38352306b58b1f
Deepak R <deepakr@carestack.com>
2023-04-11 19:09:50 +0530

    Cleanup

--- SingPass/FunctionApp1/Function1.cs
+++ SingPass/FunctionApp1/Function1.cs
@@ -8,31 +8,11 @@ namespace FunctionApp1
 {
     public static class SingpassCallback
     {
-        [FunctionName("qrCallback_callback")]
-        public static  IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "qrCallback/callback")] HttpRequest req,ILogger log)
-        {
-            log.LogInformation("Received Code in staging"+ req.Query["code"]);
-            return new OkResult();
-        }
-
-        [FunctionName("qrCallbackProd_callback")]
-        public static IActionResult RunProd([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "qrCallbackProd/callback")] HttpRequest req, ILogger log)
-        {
-            log.LogInformation("Received Code in prod" + req.Query["code"]);
-            return new OkResult();
-        }
 
         [FunctionName("qrCallback")]
         public static IActionResult Run1([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "qrCallback")] HttpRequest req, ILogger log)
         {
-            log.LogInformation("Received Code in staging 1" + req.Query["code"]);
-            return new OkResult();
-        }
-
-        [FunctionName("qrCallbackProd")]
-        public static IActionResult RunProd1([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "qrCallbackProd")] HttpRequest req, ILogger log)
-        {
-            log.LogInformation("Received Code in prod 1" + req.Query["code"]);
+            log.LogInformation("Received Code" + req.Query["code"]);
             return new OkResult();
         }
     }
